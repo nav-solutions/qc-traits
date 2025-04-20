18bd04f63e2f86751feff2e6ec5cfba3678a8950
gwbres <guillaume.bressaix@gmail.com>
2025-04-20 13:45:08 +0200

    Dep/hifitime (#13)
    * move hifitime to master branch
    * dep/hifitime integration for testing
    
    ---------
    
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/processing/time/absolute.rs
+++ src/processing/time/absolute.rs
@@ -24,8 +24,12 @@ impl GnssAbsoluteTime {
     /// Add a new [TimePolynomial] to this management pool.
     /// Usually right after its publication.
     pub fn add_polynomial(&mut self, polynomial: TimePolynomial) {
-        self.polynomials
-            .retain(|t| t.ref_epoch.time_scale != polynomial.ref_epoch.time_scale);
+        self.polynomials.retain(|poly| {
+            let same_ref = poly.ref_epoch.time_scale == polynomial.ref_epoch.time_scale;
+
+            let same_lhs = poly.ref_epoch.time_scale == polynomial.ref_epoch.time_scale;
+            !(same_ref && same_lhs)
+        });
 
         self.polynomials.push(polynomial);
     }
@@ -37,43 +41,25 @@ impl GnssAbsoluteTime {
             return Some(t);
         }
 
-        if let Some(polynomials) = self
+        if let Some(poly) = self
             .polynomials
             .iter()
             .find(|poly| poly.lhs_timescale == t.time_scale && poly.ref_epoch.time_scale == target)
         {
-            Some(polynomials.epoch_time_correction(true, t, target))
-        } else if let Some(polynomials) = self
+            Some(
+                t.precise_timescale_conversion(true, poly.ref_epoch, poly.polynomial, target)
+                    .unwrap(),
+            )
+        } else if let Some(poly) = self
             .polynomials
             .iter()
             .find(|poly| poly.ref_epoch.time_scale == t.time_scale && poly.lhs_timescale == target)
         {
-            Some(polynomials.epoch_time_correction(false, t, target))
+            Some(
+                t.precise_timescale_conversion(false, poly.ref_epoch, poly.polynomial, target)
+                    .unwrap(),
+            )
         } else {
-            //GPST-UTC=a
-            //UTC=GPST-a =  forward
-            //GPST=a+UTC = backward
-
-            // try to form a combination
-            for poly_1 in self.polynomials.iter() {
-                if poly_1.lhs_timescale == t.time_scale && poly_1.ref_epoch.time_scale != target {
-                    for poly_2 in self.polynomials.iter() {
-                        if poly_2.lhs_timescale != t.time_scale
-                            && poly_2.ref_epoch.time_scale == target
-                        {
-                            // GPST-UTC
-                            // GPST-GST=a1  GST-UTC=a2
-                            // GPST-GST    + GST-UTC  a1 + GST     + a2-GST
-                            let mut correction = poly_1.correction_duration(t);
-                            correction += poly_2.correction_duration(t);
-
-                            let converted = t.to_time_scale(target);
-                            return Some(converted + correction);
-                        }
-                    }
-                }
-            }
-
             None
         }
     }
@@ -82,7 +68,7 @@ impl GnssAbsoluteTime {
 #[cfg(test)]
 mod test {
     use crate::{GnssAbsoluteTime, TimePolynomial};
-    use hifitime::{Epoch, TimeScale};
+    use hifitime::{Duration, Epoch, Polynomial, TimeScale};
     use std::str::FromStr;
 
     #[test]
@@ -90,7 +76,11 @@ mod test {
         let polynomials = [TimePolynomial {
             lhs_timescale: TimeScale::GST,
             ref_epoch: Epoch::from_str("2020-01-01T00:00:00 GPST").unwrap(),
-            polynomials: (1.0E-9, 1.0E-10, 0.0),
+            polynomial: Polynomial {
+                constant: Duration::from_seconds(1.0E-9),
+                rate: Duration::from_seconds(1.0E-10),
+                accel: Duration::from_seconds(0.0),
+            },
         }];
 
         let solver = GnssAbsoluteTime::new(&polynomials);
@@ -117,12 +107,20 @@ mod test {
             TimePolynomial {
                 lhs_timescale: TimeScale::GST,
                 ref_epoch: Epoch::from_str("2020-01-01T00:00:00 UTC").unwrap(),
-                polynomials: (1.0E-9, 1.0E-10, 0.0),
+                polynomial: Polynomial {
+                    constant: Duration::from_seconds(1.0E-9),
+                    rate: Duration::from_seconds(1.0E-10),
+                    accel: Duration::from_seconds(0.0),
+                },
             },
             TimePolynomial {
                 lhs_timescale: TimeScale::UTC,
                 ref_epoch: Epoch::from_str("2020-01-01T00:00:00 GPST").unwrap(),
-                polynomials: (2.0E-9, 2.0E-10, 0.0),
+                polynomial: Polynomial {
+                    constant: Duration::from_seconds(2.0E-9),
+                    rate: Duration::from_seconds(2.0E-10),
+                    accel: Duration::from_seconds(0.0),
+                },
             },
         ];
 
@@ -154,11 +152,12 @@ mod test {
 
         assert_eq!(t_gpst_utc.time_scale, TimeScale::UTC);
 
-        let t_gst_gpst = solver
-            .epoch_time_correction(t_gst, TimeScale::GPST)
-            .unwrap();
+        // not feasible yet
+        // let t_gst_gpst = solver
+        //     .epoch_time_correction(t_gst, TimeScale::GPST)
+        //     .unwrap();
 
-        assert_eq!(t_gst_gpst.time_scale, TimeScale::GPST);
+        // assert_eq!(t_gst_gpst.time_scale, TimeScale::GPST);
 
         // let t_gst_gpst = solver.epoch_time_correction(t_gst, TimeScale::GPST)
         //     .unwrap();
