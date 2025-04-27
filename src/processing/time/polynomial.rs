b1e8f6d73f817eea4c91f5c32c3145734488c7a1
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-04-27 14:29:58 +0200

    GnssAbsoluteTime: indirect forward and backward transforms
      * support indirect forward and backward transforms
      * including in UTC timescale
      * add more tests
    
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/processing/time/polynomial.rs
+++ src/processing/time/polynomial.rs
@@ -4,10 +4,11 @@ use hifitime::{Epoch, Polynomial, TimeScale};
 /// For example, |[TimeScale::GPST]-[TimeScale::UTC]| when referencing [TimeScale::GPST] to [TimeScale::UTC].
 #[derive(Copy, Clone, PartialEq)]
 pub struct TimePolynomial {
-    /// lhs [TimeScale]
+    /// LHS [TimeScale] to which the [Polynomial] applies
     pub lhs_timescale: TimeScale,
-    /// Reference [Epoch] expressed in the [TimeScale] in which the polynomials apply,
-    /// with 1 ns accuracy.
+    /// RHS [TimeScale] to which the [Polynomial] applies
+    pub rhs_timescale: TimeScale,
+    /// Reference [Epoch] usually expressed in LHS [TimeScale], but we support any [TimeScale] here.
     pub ref_epoch: Epoch,
     /// [Polynomial]
     pub polynomial: Polynomial,
@@ -17,17 +18,17 @@ impl TimePolynomial {
     /// Define new [TimePolynomial] from Reference [Epoch] expressed as week counter
     /// and elapsed seconds within week.
     pub fn from_reference_time_of_week_seconds(
-        lhs_timescale: TimeScale,
         ref_week: u32,
         ref_tow: u64,
-        ref_timescale: TimeScale,
+        lhs_timescale: TimeScale,
+        rhs_timescale: TimeScale,
         polynomial: Polynomial,
     ) -> Self {
         Self::from_reference_time_of_week_nanos(
-            lhs_timescale,
             ref_week,
             ref_tow * 1_000_000_000,
-            ref_timescale,
+            lhs_timescale,
+            rhs_timescale,
             polynomial,
         )
     }
@@ -35,17 +36,18 @@ impl TimePolynomial {
     /// Define a new [TimePolynomials] from Reference [Epoch] expressed as week counter and
     /// elapsed nanoseconds within week.
     pub fn from_reference_time_of_week_nanos(
-        lhs_timescale: TimeScale,
         ref_week: u32,
-        ref_tow: u64,
-        ref_timescale: TimeScale,
+        ref_tow_nanos: u64,
+        lhs_timescale: TimeScale,
+        rhs_timescale: TimeScale,
         polynomial: Polynomial,
     ) -> Self {
-        let ref_epoch = Epoch::from_time_of_week(ref_week, ref_tow, ref_timescale);
+        let ref_epoch = Epoch::from_time_of_week(ref_week, ref_tow_nanos, lhs_timescale);
 
         Self {
             ref_epoch,
             lhs_timescale,
+            rhs_timescale,
             polynomial,
         }
     }
