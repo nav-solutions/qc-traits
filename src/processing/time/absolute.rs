a8eaaa70ec2c427cfe4da25cd1b1f8e8d5f733f2
gwbres <guillaume.bressaix@gmail.com>
2025-04-27 14:32:43 +0200

    GnssAbsoluteTime: indirect forward and backward transforms (#15)
    * support indirect forward and backward transforms
      * including in UTC timescale
      * add more tests
    
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/processing/time/absolute.rs
+++ src/processing/time/absolute.rs
@@ -1,15 +1,16 @@
 use super::TimePolynomial;
 
-use hifitime::{Epoch, TimeScale};
+use hifitime::{Duration, Epoch, TimeScale};
 
 #[cfg(doc)]
 use super::Timeshift;
 
-/// [GnssAbsoluteTime] is used by the [Timeshift] trait and applications
-/// that need to manage several timescales and allow correction conversion
-/// back & forth at all times.
+/// [GnssAbsoluteTime] is used by applications that need to precisely describe
+/// the actual state of one or several [TimeScale]s. It is used in our [Timeshift] trait
+/// to allow precise [TimeScale] transposition.
 #[derive(Default)]
 pub struct GnssAbsoluteTime {
+    /// Internal [TimePolynomial]s
     polynomials: Vec<TimePolynomial>,
 }
 
@@ -35,16 +36,11 @@ impl GnssAbsoluteTime {
     }
 
     /// [Epoch] interpolation & correction attempt, into desired [TimeScale].
-    pub fn epoch_time_correction(&self, t: Epoch, target: TimeScale) -> Option<Epoch> {
-        // default case:
-        if t.time_scale == target {
-            return Some(t);
-        }
-
+    pub fn precise_epoch_correction(&self, t: Epoch, target: TimeScale) -> Option<Epoch> {
         if let Some(poly) = self
             .polynomials
             .iter()
-            .find(|poly| poly.lhs_timescale == t.time_scale && poly.ref_epoch.time_scale == target)
+            .find(|poly| poly.lhs_timescale == t.time_scale && poly.rhs_timescale == target)
         {
             Some(
                 t.precise_timescale_conversion(true, poly.ref_epoch, poly.polynomial, target)
@@ -53,13 +49,92 @@ impl GnssAbsoluteTime {
         } else if let Some(poly) = self
             .polynomials
             .iter()
-            .find(|poly| poly.ref_epoch.time_scale == t.time_scale && poly.lhs_timescale == target)
+            .find(|poly| poly.rhs_timescale == t.time_scale && poly.lhs_timescale == target)
         {
             Some(
                 t.precise_timescale_conversion(false, poly.ref_epoch, poly.polynomial, target)
                     .unwrap(),
             )
         } else {
+            for lhs_poly in self.polynomials.iter() {
+                for rhs_poly in self.polynomials.iter() {
+                    if lhs_poly.lhs_timescale == t.time_scale && rhs_poly.rhs_timescale == target {
+                        // indirect forward transforms
+
+                        // |BDT-GST|=a0_bdt & |GST-GPST|=a1 dt_gst
+                        // GST=BDT-a0_bdt
+                        // BDT-a0 dt_bdt - GPST = a1 dt_gpst
+                        // BDT-GPST (foward indirect) = a1 dt_gpst + a0 dt_bdt
+
+                        let dt_lhs_s = (t.to_time_scale(lhs_poly.lhs_timescale)
+                            - lhs_poly.ref_epoch)
+                            .to_seconds();
+                        let dt_rhs_s = (t.to_time_scale(rhs_poly.lhs_timescale)
+                            - rhs_poly.ref_epoch)
+                            .to_seconds();
+
+                        let mut correction = lhs_poly.polynomial.constant.to_seconds()
+                            + lhs_poly.polynomial.rate.to_seconds() * dt_lhs_s
+                            + lhs_poly.polynomial.accel.to_seconds() * dt_lhs_s.powi(2);
+
+                        // println!("correction = {}", correction);
+
+                        correction += rhs_poly.polynomial.constant.to_seconds()
+                            + rhs_poly.polynomial.rate.to_seconds() * dt_rhs_s
+                            + rhs_poly.polynomial.accel.to_seconds() * dt_rhs_s.powi(2);
+
+                        // println!("total correction = {}", correction);
+
+                        return Some(
+                            t.to_time_scale(rhs_poly.rhs_timescale)
+                                - Duration::from_seconds(correction),
+                        );
+                    } else if lhs_poly.rhs_timescale == t.time_scale
+                        && rhs_poly.rhs_timescale == target
+                    {
+                        // indirect backward + forward transforms
+                    } else if lhs_poly.lhs_timescale == t.time_scale
+                        && rhs_poly.lhs_timescale == target
+                    {
+                        // indirect forward + backward transforms
+                    } else if lhs_poly.rhs_timescale == t.time_scale
+                        && rhs_poly.lhs_timescale == target
+                    {
+                        // indirect backward transforms
+
+                        // |BDT-GST|=a0_bdt & |GST-GPST|=a1 dt_gst
+                        // BDT  = a0_bdt + GST
+                        // GPST = GST -a1 dt_gpst
+                        // GPST-BDT (backward indirect) = -a1 -a0
+
+                        let dt_lhs_s = (t.to_time_scale(lhs_poly.lhs_timescale)
+                            - lhs_poly.ref_epoch)
+                            .to_seconds();
+                        let dt_rhs_s = (t.to_time_scale(rhs_poly.lhs_timescale)
+                            - rhs_poly.ref_epoch)
+                            .to_seconds();
+
+                        let correction_a = lhs_poly.polynomial.constant.to_seconds()
+                            + lhs_poly.polynomial.rate.to_seconds() * dt_lhs_s
+                            + lhs_poly.polynomial.accel.to_seconds() * dt_lhs_s.powi(2);
+
+                        // println!("correction = {}", correction_a);
+
+                        let correction_b = rhs_poly.polynomial.constant.to_seconds()
+                            + rhs_poly.polynomial.rate.to_seconds() * dt_rhs_s
+                            + rhs_poly.polynomial.accel.to_seconds() * dt_rhs_s.powi(2);
+
+                        // println!("correction = {}", correction_b);
+
+                        return Some(
+                            t.to_time_scale(rhs_poly.lhs_timescale)
+                                + Duration::from_seconds(correction_a)
+                                + Duration::from_seconds(correction_b),
+                        );
+                    }
+                }
+            }
+
             None
         }
     }
@@ -73,100 +148,251 @@ mod test {
 
     #[test]
     fn test_direct_absolute_time() {
+        let t_ref_gpst = Epoch::from_str("2020-01-01T00:00:00 GPST").unwrap();
+
+        let (a0, _, _) = (1.0E-9, 0.0, 0.0);
+
+        let polynomial = Polynomial {
+            constant: Duration::from_seconds(a0),
+            rate: Duration::ZERO,
+            accel: Duration::ZERO,
+        };
+
         let polynomials = [TimePolynomial {
             lhs_timescale: TimeScale::GST,
-            ref_epoch: Epoch::from_str("2020-01-01T00:00:00 GPST").unwrap(),
-            polynomial: Polynomial {
-                constant: Duration::from_seconds(1.0E-9),
-                rate: Duration::from_seconds(1.0E-10),
-                accel: Duration::from_seconds(0.0),
-            },
+            rhs_timescale: TimeScale::GPST,
+            ref_epoch: t_ref_gpst,
+            polynomial,
         }];
 
         let solver = GnssAbsoluteTime::new(&polynomials);
 
-        let t_gst = Epoch::from_str("2020-01-01T00:00:10 GST").unwrap();
-        let t_gpst = Epoch::from_str("2020-01-01T00:00:10 GPST").unwrap();
+        // Random date in GST
+        let t_gst = Epoch::from_str("2020-01-01T00:00:00 GST").unwrap();
 
         let t_gst_gpst = solver
-            .epoch_time_correction(t_gst, TimeScale::GPST)
+            .precise_epoch_correction(t_gst, TimeScale::GPST)
             .unwrap();
 
         assert_eq!(t_gst_gpst.time_scale, TimeScale::GPST);
 
+        // Random date in GPST
+        let t_gpst = Epoch::from_str("2020-01-01T00:00:10 GPST").unwrap();
+
         let t_gpst_gst = solver
-            .epoch_time_correction(t_gpst, TimeScale::GST)
+            .precise_epoch_correction(t_gpst, TimeScale::GST)
             .unwrap();
 
         assert_eq!(t_gpst_gst.time_scale, TimeScale::GST);
+
+        // Random date in UTC
+        let t_utc = Epoch::from_str("2020-01-01T00:00:10 UTC").unwrap();
+
+        assert!(solver
+            .precise_epoch_correction(t_utc, TimeScale::GST)
+            .is_none());
+
+        assert!(solver
+            .precise_epoch_correction(t_utc, TimeScale::GPST)
+            .is_none());
     }
 
     #[test]
-    fn test_indirect_absolute_time() {
+    fn test_indirect_forward_transform_not_utc() {
+        let t_ref_bdt = Epoch::from_str("2020-01-01T00:00:00 BDT").unwrap();
+        let t_ref_gst = Epoch::from_str("2020-01-01T00:00:00 GST").unwrap();
+        //let t_ref_gpst = Epoch::from_str("2020-01-01T00:00:00 GPST").unwrap();
+
+        let (a0_bdt_gst, _, _) = (1.0E-9, 0.0, 0.0);
+        let (a0_gst_gpst, _, _) = (2.0E-9, 0.0, 0.0);
+
         let polynomials = [
             TimePolynomial {
-                lhs_timescale: TimeScale::GST,
-                ref_epoch: Epoch::from_str("2020-01-01T00:00:00 UTC").unwrap(),
+                lhs_timescale: TimeScale::BDT,
+                rhs_timescale: TimeScale::GST,
+                ref_epoch: t_ref_bdt,
                 polynomial: Polynomial {
-                    constant: Duration::from_seconds(1.0E-9),
-                    rate: Duration::from_seconds(1.0E-10),
-                    accel: Duration::from_seconds(0.0),
+                    constant: Duration::from_seconds(a0_bdt_gst),
+                    rate: Duration::ZERO,
+                    accel: Duration::ZERO,
                 },
             },
             TimePolynomial {
-                lhs_timescale: TimeScale::UTC,
-                ref_epoch: Epoch::from_str("2020-01-01T00:00:00 GPST").unwrap(),
+                lhs_timescale: TimeScale::GST,
+                rhs_timescale: TimeScale::GPST,
+                ref_epoch: t_ref_gst,
                 polynomial: Polynomial {
-                    constant: Duration::from_seconds(2.0E-9),
-                    rate: Duration::from_seconds(2.0E-10),
-                    accel: Duration::from_seconds(0.0),
+                    constant: Duration::from_seconds(a0_gst_gpst),
+                    rate: Duration::ZERO,
+                    accel: Duration::ZERO,
                 },
             },
         ];
 
         let solver = GnssAbsoluteTime::new(&polynomials);
 
-        let t_gst = Epoch::from_str("2020-01-01T00:00:10 GST").unwrap();
-        let t_utc = Epoch::from_str("2020-01-01T00:00:10 UTC").unwrap();
-        let t_gpst = Epoch::from_str("2020-01-01T00:00:10 GPST").unwrap();
+        // verify direct transforms still work
+        let t_gst = Epoch::from_str("2020-01-01T00:00:00 GST").unwrap();
+        let t_bdt = Epoch::from_str("2020-01-01T00:00:00 BDT").unwrap();
+        let t_gpst = Epoch::from_str("2020-01-01T00:00:00 GPST").unwrap();
 
-        let t_gst_utc = solver.epoch_time_correction(t_gst, TimeScale::UTC).unwrap();
-
-        assert_eq!(t_gst_utc.time_scale, TimeScale::UTC);
-
-        let t_utc_gpst = solver
-            .epoch_time_correction(t_utc, TimeScale::GPST)
+        let t_gst_gpst = solver
+            .precise_epoch_correction(t_gst, TimeScale::GPST)
             .unwrap();
 
-        assert_eq!(t_utc_gpst.time_scale, TimeScale::GPST);
+        assert_eq!(t_gst_gpst.time_scale, TimeScale::GPST);
 
-        let t_utc_gpst = solver
-            .epoch_time_correction(t_utc, TimeScale::GPST)
+        let t_gpst_gst = solver
+            .precise_epoch_correction(t_gpst, TimeScale::GST)
             .unwrap();
 
-        assert_eq!(t_utc_gpst.time_scale, TimeScale::GPST);
+        assert_eq!(t_gpst_gst.time_scale, TimeScale::GST);
+
+        let t_gst_bdt = solver
+            .precise_epoch_correction(t_gst, TimeScale::BDT)
+            .unwrap();
+
+        assert_eq!(t_gst_bdt.time_scale, TimeScale::BDT);
+
+        let t_bdt_gst = solver
+            .precise_epoch_correction(t_bdt, TimeScale::GST)
+            .unwrap();
+
+        assert_eq!(t_bdt_gst.time_scale, TimeScale::GST);
+
+        // indirect forward transform
+        let t_bdt_gpst = solver
+            .precise_epoch_correction(t_bdt, TimeScale::GPST)
+            .unwrap();
+
+        assert_eq!(t_bdt_gpst.time_scale, TimeScale::GPST);
+
+        let coarsed = t_bdt.to_time_scale(TimeScale::GPST);
+        let dt = coarsed - t_bdt_gpst;
+
+        assert_eq!(
+            dt,
+            Duration::from_seconds(a0_bdt_gst) + Duration::from_seconds(a0_gst_gpst)
+        );
+
+        // linearity
+        let reciprocal = solver
+            .precise_epoch_correction(t_bdt_gpst, TimeScale::BDT)
+            .unwrap();
+
+        assert_eq!(reciprocal.time_scale, TimeScale::BDT);
+        assert_eq!(reciprocal, t_bdt);
+
+        // indirect backward transform
+        let t_gpst_bdt = solver
+            .precise_epoch_correction(t_gpst, TimeScale::BDT)
+            .unwrap();
+
+        assert_eq!(t_gpst_bdt.time_scale, TimeScale::BDT);
+
+        let coarsed = t_gpst.to_time_scale(TimeScale::BDT);
+        let dt = coarsed - t_gpst_bdt;
+
+        assert_eq!(
+            dt,
+            Duration::from_seconds(a0_bdt_gst) + Duration::from_seconds(a0_gst_gpst)
+        );
+
+        // linearity
+        let reciprocal = solver
+            .precise_epoch_correction(t_gpst_bdt, TimeScale::GPST)
+            .unwrap();
+
+        assert_eq!(reciprocal.time_scale, TimeScale::GPST);
+        assert_eq!(reciprocal, t_gpst);
+    }
+
+    #[test]
+    fn test_indirect_forward_transform_utc() {
+        let t_ref_bdt = Epoch::from_str("2020-01-01T00:00:00 BDT").unwrap();
+        //let t_ref_gst = Epoch::from_str("2020-01-01T00:00:00 GST").unwrap();
+        let t_ref_gpst = Epoch::from_str("2020-01-01T00:00:00 GPST").unwrap();
+
+        let (a0_bdt_gst, _, _) = (1.0E-9, 0.0, 0.0);
+        let (a0_gpst_utc, _, _) = (2.0E-9, 0.0, 0.0);
+
+        let polynomials = [
+            TimePolynomial {
+                lhs_timescale: TimeScale::BDT,
+                rhs_timescale: TimeScale::GST,
+                ref_epoch: t_ref_bdt,
+                polynomial: Polynomial {
+                    constant: Duration::from_seconds(a0_bdt_gst),
+                    rate: Duration::ZERO,
+                    accel: Duration::ZERO,
+                },
+            },
+            TimePolynomial {
+                lhs_timescale: TimeScale::GPST,
+                rhs_timescale: TimeScale::UTC,
+                ref_epoch: t_ref_gpst,
+                polynomial: Polynomial {
+                    constant: Duration::from_seconds(a0_gpst_utc),
+                    rate: Duration::ZERO,
+                    accel: Duration::ZERO,
+                },
+            },
+        ];
+
+        let solver = GnssAbsoluteTime::new(&polynomials);
+
+        // verify direct transforms still work
+        let t_gst = Epoch::from_str("2020-01-01T00:00:00 GST").unwrap();
+        let t_bdt = Epoch::from_str("2020-01-01T00:00:00 BDT").unwrap();
+        let t_gpst = Epoch::from_str("2020-01-01T00:00:00 GPST").unwrap();
 
         let t_gpst_utc = solver
-            .epoch_time_correction(t_gpst, TimeScale::UTC)
+            .precise_epoch_correction(t_gpst, TimeScale::UTC)
             .unwrap();
 
         assert_eq!(t_gpst_utc.time_scale, TimeScale::UTC);
 
-        // not feasible yet
-        // let t_gst_gpst = solver
-        //     .epoch_time_correction(t_gst, TimeScale::GPST)
-        //     .unwrap();
+        // linearity
+        let reciprocal = solver
+            .precise_epoch_correction(t_gpst_utc, TimeScale::GPST)
+            .unwrap();
 
-        // assert_eq!(t_gst_gpst.time_scale, TimeScale::GPST);
+        assert_eq!(reciprocal.time_scale, TimeScale::GPST);
+        assert_eq!(reciprocal, t_gpst);
 
-        // let t_gst_gpst = solver.epoch_time_correction(t_gst, TimeScale::GPST)
-        //     .unwrap();
+        let t_gst_bdt = solver
+            .precise_epoch_correction(t_gst, TimeScale::BDT)
+            .unwrap();
 
-        // assert_eq!(t_gst_gpst.time_scale, TimeScale::GPST);
+        assert_eq!(t_gst_bdt.time_scale, TimeScale::BDT);
 
-        // let t_gpst_gst = solver.epoch_time_correction(t_gpst, TimeScale::GST)
-        //     .unwrap();
+        let t_bdt_gst = solver
+            .precise_epoch_correction(t_bdt, TimeScale::GST)
+            .unwrap();
 
-        // assert_eq!(t_gpst_gst.time_scale, TimeScale::GST);
+        assert_eq!(t_bdt_gst.time_scale, TimeScale::GST);
+
+        // indirect forward transform
+        let t_bdt_utc = solver
+            .precise_epoch_correction(t_bdt, TimeScale::UTC)
+            .unwrap();
+
+        assert_eq!(t_bdt_utc.time_scale, TimeScale::UTC);
+
+        let coarsed = t_bdt.to_time_scale(TimeScale::UTC);
+        let dt = coarsed - t_bdt_utc;
+
+        assert_eq!(
+            dt,
+            Duration::from_seconds(a0_bdt_gst) + Duration::from_seconds(a0_gpst_utc)
+        );
+
+        // linearity
+        let reciprocal = solver
+            .precise_epoch_correction(t_bdt_utc, TimeScale::BDT)
+            .unwrap();
+
+        assert_eq!(reciprocal.time_scale, TimeScale::BDT);
+        assert_eq!(reciprocal, t_bdt);
     }
 }
