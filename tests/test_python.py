fcdc3d1c6e8b463ed047930d985c8645641ac14e
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-12-06 11:58:05 +0100

    python bindings
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- tests/test_python.py
+++ tests/test_python.py
@@ -3,5 +3,5 @@ from qc_traits import Epoch, TimeScale, Duration, Polynomial, TimeCorrection, Ti
 def test_py():
     refepoch = Epoch("2020-01-01T00:00:00 UTC")
     (lhs, rhs) = (TimeScale.UTC, TimeScale.GPST)
-    polynomial = Polynomial.from_constant_offset(Duration.from_nanoseconds(10))
+    polynomial = Polynomial.from_constant_offset_nanoseconds(10)
     tc = TimeCorrection(lhs, rhs, refepoch, polynomial)
